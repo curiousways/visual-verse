bede0977266499caae8c5b09da0e85483942009f
Matt Beard <matt@mattbeard.co.uk>
2021-03-16 15:12:53 +0000

    accordion A-Z authors and artists on archive page.

--- partials/list-authors.php
+++ partials/list-authors.php
@@ -1,17 +1,53 @@
 <div class="list-sidebar">
 	<h3>Authors</h3>
 	<nav>
-		<?php 
-		$terms = get_terms('writers', 'number=10000');
-		echo '<ul>';
-		foreach ($terms as $term) {
+
+
+<?php
+$terms = get_terms('writers');
+
+
+
+if ( !empty( $terms ) && !is_wp_error( $terms ) ){    
+$term_list = [];    
+foreach ( $terms as $term ){
+
+    $first_letter = sanitize_title($term->name[0]);
+    $term_list[$first_letter][] = $term;
+	
+}
+
+echo '<div class="my_term-archive">';
+
+    foreach ( $term_list as $key=>$value ) {
+		echo '<details>';
+        echo '<summary style="margin-top: 15px; margin-bottom: 15px; padding: 10px 10px 7px 10px; background-color: grey; border-radius: 10px; text-transform: uppercase;">' . $key . '</summary>';
+
+        foreach ( $value as $term ) {
+
 		    $term_link = get_term_link( $term, 'writers' );
 		    if( is_wp_error( $term_link ) )
 		        continue;
-		    echo '<li><a href="' . $term_link . '">' . $term->name . '</a></li>';
-		      //echo '<li>' . $term->name . '</li>';
-		}
-		echo '</ul>'; 
-		?>
+
+           echo '<li style="list-style-type: none"><a href="' . $term_link . '">' . $term->name . '</a></li>';
+        }
+		echo '</details>';
+    }
+
+echo '</div>';
+} 
+
+  ?>
+ 
+
+
+
+
 	</nav>
 </div>
+
+
+<!-- <details>
+    <summary>Details</summary>
+    Something small enough to escape casual notice.
+</details> -->
